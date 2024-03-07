431f956808381dd156125816eb38cfca671ff5e5
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-07 14:31:07 -0600

    removed console log

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import questions from './Questions'
 
 function App() {
 
-
   const [currentCard, setCurrentCard] = useState(0)
   const [showAnswer, setShowAnswer] = useState(false)
 
@@ -24,14 +23,12 @@ function App() {
     }
   }
 
-
   return (
     <div className='container'>
       <h1>Psychology Facts</h1>
       <h3>Want to test your psychology knowledge? Click the button below!</h3>
       <h4>There are { questions.length } cards in this quiz</h4>
       <div className='question-box' onClick={ handleClick }>
-        <h2>{ console.log(showAnswer) }</h2>
         { !showAnswer && <h2>{ questions[currentCard].question }</h2> }
         { showAnswer && <h2>{ questions[currentCard].answer }</h2> }
       </div>
