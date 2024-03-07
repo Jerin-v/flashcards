4ac278ccc283853e2d9431d03691861d8a5e5486
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-07 13:12:42 -0600

    created questions array

---
+++ src/Questions.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const questions = [
+    {question: "who folded yesterday?", answer: "Jerin"},
+    {question: "who may fold today?", answer: "Jerin again"},
+    {question: "who's gonna keep folding till they figure this out?", answer: "Guess"}, 
+    {question: "What is the solution to your current problem?", answer: "Action"}
+]
+
+export default questions;
