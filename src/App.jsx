d17742dbf98d342cefba7274cae59987522d6582
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-07 13:12:27 -0600

    linked questions array to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import { useState } from 'react'
 import './App.css'
-import Card from './Card'
+import questions from './Questions'
+
 
 
 function App() {
 
 
-  const [count, setCount] = useState(0)
+  const [currentCard, setCurrentCard] = useState(0)
+
+  const nextCard = () => {
+    const randomIndex = Math.floor(Math.random() * questions.length)
+    setCurrentCard(randomIndex)
+  }
+
 
   return (
     <div className='container'>
       <h1>Psychology Facts</h1>
       <h3>Want to test your psychology knowledge? Click the button below!</h3>
       <div className='question-box'>
-        <Card />
+        <h2 className='questions'>{ questions[currentCard].question }</h2>
+
+        <h2 className='answer'>{ questions[currentCard].answer }</h2>
       </div>
+
+      <button onClick={nextCard}>
+          Next
+        </button>
 
     </div>
     
