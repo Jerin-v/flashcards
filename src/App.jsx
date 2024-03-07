4910898775adadbb3c56b011a08945729b6d4f77
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-07 14:00:22 -0600

    added handle click function and answer usestate

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,30 @@ function App() {
 
 
   const [currentCard, setCurrentCard] = useState(0)
+  const [showAnswer, setShowAnswer] = useState(false)
 
   const nextCard = () => {
     const randomIndex = Math.floor(Math.random() * questions.length)
     setCurrentCard(randomIndex)
+    setShowAnswer(false)
+  }
+
+  const handleClick = () => {
+    if(!showAnswer){
+      setShowAnswer(true)
+    } else {
+      setShowAnswer(false)
+    }
+  }
+
+  const showCard = () => {
+    if(!showAnswer) {
+      return 
+      { questions[currentCard].question }
+    } else {
+      return
+      <h2>{ questions[currentCard].answer }</h2>
+    }
   }
 
 
@@ -19,10 +39,8 @@ function App() {
     <div className='container'>
       <h1>Psychology Facts</h1>
       <h3>Want to test your psychology knowledge? Click the button below!</h3>
-      <div className='question-box'>
-        <h2 className='questions'>{ questions[currentCard].question }</h2>
-
-        <h2 className='answer'>{ questions[currentCard].answer }</h2>
+      <div className='question-box' onClick={ handleClick }>
+        <h2>{ console.log(showAnswer) }</h2>
       </div>
 
       <button onClick={nextCard}>
