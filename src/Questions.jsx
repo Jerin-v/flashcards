29611e343f5445e1128097e1ba0319c238e0df02
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-08 22:18:53 -0600

    updated question bank

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 const questions = [
-    {question: "who folded yesterday?", answer: "Jerin"},
-    {question: "who may fold today?", answer: "Jerin again"},
-    {question: "who's gonna keep folding till they figure this out?", answer: "Guess"}, 
-    {question: "What is the solution to your current problem?", answer: "Action"}
+    {question: "Psychology", answer: "The scientific study of behavior and mental processes."},
+    {question: "Behavior", answer: "All outward or overt actions and reactions."},
+    {question: "structuralism", answer: "Focus of the study is the structure or basic elements of the mind."}, 
+    {question: "correlation", answer: "A measure of the relationship between two variables"},
+    {question: "single-blind study", answer: "study in which the subjects do not know if they are in the experimental or the control group"}, 
+    {question: "double-blind study", answer: "study in which neither the experimenter nor the subjects know if the subjects are in the experimental or control group"}, 
+    {question: "dopamine", answer: "attention, learning, movement, cocaine, schizophrenia"}, 
+    {question: "serotonin", answer: "mood, sleep, appetite, aggression, depression [too little]"}, 
+    {question: "frontal lobe", answer: "emotions; personality, decisions, memory, planning, inhibition"}, 
+    {question: "neurons", answer: "soma has the dendrites around it which receives information and send it to the synaptic knobs and turns it into a chemical message"}, 
 ]
 
 export default questions;
